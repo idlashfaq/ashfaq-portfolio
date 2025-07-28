8d0b85aaff8b768119b4c18b1ad6637cf29fd565
ashfaq <ashfaq@carswitch.com>
2025-07-28 19:43:54 +0500

    added project files

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    main_scripts.init();
+});
+
+var main_scripts = {
+    init: function(){
+        new WOW().init();
+        $('.testimonials-items-holder').slick();
+        // $('.recent-work .cards-holder').slick();
+
+        $(window).scroll(function(){
+            let scroll = $(window).scrollTop();
+            if(scroll > 100){
+                $('.main-navbar').addClass("active")
+            } else {
+                $('.main-navbar').removeClass("active")
+            }
+        });
+
+        $(document).on('click', '.main-navbar .hamburger-icon', function(){
+            $("body").addClass('show-menu');
+        })
+        $(document).on('click', '.main-navbar .close-icon', function(){
+            $("body").removeClass('show-menu');
+        })
+
+        this.bindings();
+    },
+}
+
+$(document).ready(function(e) {
+   
+	lc_lightbox('.elem', {
+		gallery : true,	
+		thumbs_nav: false,
+        nav_btn_pos: false,
+		skin: 'minimal',
+		radius: 0,
+		padding	: 0,
+		border_w: 0,
+	});	
+
+});
